c1d76745a2f608650a50e0adff68a7228cd25129
Micah J <36313854+justmic007@users.noreply.github.com>
2022-05-31 12:09:05 +0100

    setup db configs (#1)
    Co-authored-by: Micah J <justmic007>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: ['dist/src/**/*.entity.js'],
+      synchronize: true, // set to FALSE in Production
+      migrations: ['dist/src/persistence/migrations/*.js'],
+      // cli: {
+      //   migrationsDir: 'dist/src/persistence/migrations'
+      // }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
